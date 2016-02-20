16887a6035b56b1d554ffefb98f15c005b30ef90
omonk <ollie@olliemonk.com>
2016-02-20 13:45:14 +0000

    add mouseover effect, update html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,41 @@
+
+// Check for touch devices
+function is_touch_device() {
+  return 'ontouchstart' in window        // works on most browsers
+      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+};
+
 jQuery(document).ready(function($){
-	console.log('ready');
-	// Please annotate your JS so we can see what you’re thinking
+
+	// Create mouseover function
+	var el = document.getElementsByClassName('headshot-item'); //get all headshot elements
+
+	// Check to see if we're on a touch device or not
+	if (!is_touch_device()) {
+		for (var i = 0; i < el.length; i++) {
+			// Attach our event listeners
+			el[i].addEventListener("mousemove", function(e) {
+
+				// get width of our headshot element
+				var x = e.pageX - $(this).offset().left;
+
+				// get all img children
+				var images = this.getElementsByTagName('img');
+
+				var oneThird = this.offsetWidth / 3;
+				var twoThird = oneThird * 2;
+
+				if (x <= oneThird) {
+					$(images).not(this).css('display', 'none');
+					$(images[0]).css('display', 'block');
+				} else if (x >= oneThird && x <= twoThird) {
+					$(images).not(this).css('display', 'none');
+					$(images[1]).css('display', 'block');
+				} else if (x >= twoThird) {
+					$(images).not(this).css('display', 'none');
+					$(images[2]).css('display', 'block');
+				}
+			});
+		}
+	}
 });
